aa6e6f0b68462c14c3add59d0543ca611d88098e
SummerNgo <hanhnguyen0301@gmail.com>
2020-06-30 22:24:34 +0800

    Summer edit : - LeaveRequestRepo - LeaveRequestService - LeaveRequestServiceImpl -ManageLeaveToApproveList.html -ManagerLeaveController

--- Team4CA_2906/src/main/java/Team4CA/LMS/repo/LeaveRequestRepo.java
+++ Team4CA_2906/src/main/java/Team4CA/LMS/repo/LeaveRequestRepo.java
@@ -1,9 +1,12 @@
 package Team4CA.LMS.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Team4CA.LMS.domain.LeaveRequest;
+import Team4CA.LMS.domain.LeaveStatus;
 
 public interface LeaveRequestRepo extends JpaRepository<LeaveRequest, Integer> {
-
+	public ArrayList<LeaveRequest> findByLeaveStatus(LeaveStatus ls);
 }
--- Team4CA_2906/src/main/java/Team4CA/LMS/service/LeaveRequestService.java
+++ Team4CA_2906/src/main/java/Team4CA/LMS/service/LeaveRequestService.java
@@ -1,5 +1,11 @@
 package Team4CA.LMS.service;
 
-public interface LeaveRequestService {
+import java.util.ArrayList;
 
+import Team4CA.LMS.domain.LeaveRequest;
+import Team4CA.LMS.domain.LeaveStatus;
+
+
+public interface LeaveRequestService {
+	public ArrayList<LeaveRequest> findByLeaveStatus(LeaveStatus ls);
 }
